8deccf8aa72a7c73dadc66f576297934aa081df6
Yupiter78 <yu.piter.gm@gmail.com>
2020-09-09 19:45:06 +0300

    solving the problem of calculating the factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,16 @@
  * @param {number} n
  * @returns {number}
  */
-function factorial(n) {
-  // ваш код...
-}
+ function factorial(n) {
+         let x = 1;
+
+         if (n <= 1) {
+             return x;
+         } else {
+             while (n > 1) {
+                 x = x * n;
+                 n = n - 1;
+             }
+             return x;
+         }
+ }
