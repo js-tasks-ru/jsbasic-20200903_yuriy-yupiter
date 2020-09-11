c75ad1d532758ddf91493dd2e29fdd13d5be88bf
Yupiter78 <yu.piter.gm@gmail.com>
2020-09-11 16:20:25 +0300

    Spam check

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,11 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+  let spam1 = "1xBet";
+  let spam2 = "XXX";
+  spam1 = spam1.toUpperCase();
+  spam2 = spam2.toUpperCase();
+  str = str.toUpperCase();
+  let spam = str.indexOf(spam1) != -1 || str.indexOf(spam2) != -1;
+  return spam;
 }
