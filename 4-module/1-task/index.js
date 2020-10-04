8727427b73cfa5d80d9488306fff2b04b18802ef
Yupiter78 <yu.piter.gm@gmail.com>
2020-10-04 23:51:09 +0300

    function makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,22 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('ul');
+
+  let nameFriend = friends.map(user => 
+    `${user.firstName} ${user.lastName}`
+         )
+
+    .forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = item;
+
+    if (item.children) {
+      li.appendChild(makeFriendsList(item.children));
+    }
+
+    ul.appendChild(li);
+  });
+
+  return ul;
 }
