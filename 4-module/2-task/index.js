28b85a5bc14d15fe3d5992513e4b3e554a146278
Yupiter78 <yu.piter.gm@gmail.com>
2020-10-05 15:28:48 +0300

    function makeDiagonalRed

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,8 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+    for (let i = 0; i < table.rows.length; i++) {
+        let row = table.rows[i];
+        row.cells[i].style.backgroundColor = 'red';
+        }
 }
