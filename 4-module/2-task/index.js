7079cabb06b80b512839954eb99a9d31266d6f92
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-10-06 11:17:41 +0300

    Added teacher solution for task: Выделите ячейки по диагонали

---
+++ 4-module/2-task/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {HTMLTableElement} table
+ * @return {void}
+ */
+function makeDiagonalRed(table) {
+  let rowsLength = table.rows.length;
+  let rows = table.rows;
+
+  for (let i = 0; i < rowsLength; i++) {
+    let row = rows[i];
+    row.cells[i].style.backgroundColor = 'red';
+  }
+}
