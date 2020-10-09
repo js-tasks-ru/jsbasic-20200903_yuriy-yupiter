01ed6812a181d02930d026be4e93243e13a13351
Yupiter78 <yu.piter.gm@gmail.com>
2020-10-09 12:17:14 +0300

    function highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,17 @@
  * @param {Element} table
  */
 function highlight(table) {
+    function highlight(table) {
+        for (let i = 1; i < table.rows.length; i++) {
+        let row = table.rows[i];
+        
+        if (row.cells[3].dataset.available === "true") {
+          row.classList.add('available');
+        } else if (row.cells[3].dataset.available === "false") {
+          row.classList.add('unavailable');
+        } else {
+          row.setAttribute('hidden', true);
+        }
+        }
+      }
 }
