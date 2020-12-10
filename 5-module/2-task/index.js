1f6789b677e252df54ab9198d6085c6efb1e3417
Yupiter78 <yu.piter.gm@gmail.com>
2020-12-10 11:59:55 +0300

    Carousel

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,9 +1,8 @@
 function toggleText() {
-  document.addEventListener('click', function(event) {
-    let hiderOne = document.querySelectorAll(".toggle-text-button")[0];
+  let hiderOne = document.querySelector(".toggle-text-button");
+  let elem = document.getElementById("text");
+  hiderOne.addEventListener('click', function() {
 
-    let elem = document.getElementById("text");
-
-    elem.hidden = !elem.hidden;
+      elem.hidden = !elem.hidden;
   });
 }
